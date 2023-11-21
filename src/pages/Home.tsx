f680c285b2ff6f8f48da3d4d92dcb311610b63fd
KIMGEUNDU <vlkoklv@naver.com>
2023-11-21 17:44:49 +0900

    docs: Home 컴포넌트 수정
    related to: #3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,13 @@
+import BestItem from '@/components/BestItem';
+import ProductContainer from '@/components/ProductContainer';
+import SlideContainer from '@/components/SlideContainer';
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <SlideContainer />
+      <BestItem title="New Item" />
+      <ProductContainer title="ALL ARRIVAL" />
+    </>
+  );
 }
