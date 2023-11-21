00068c6ff761ce88ef4482488306870b69ca0a4e
KIMGEUNDU <vlkoklv@naver.com>
2023-11-21 17:40:25 +0900

    ts: 메인페이지 인터페이스 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ContainerTitle {
+  title: string;
+}
+
+interface ProductItem {
+  link: string;
+  src: string;
+  title: string;
+  price: string;
+}
