f09ab51aab960be9a5ce93811cf7a4dba0145313
KIMGEUNDU <vlkoklv@naver.com>
2023-11-22 13:15:27 +0900

    style: 카트, 찜, 상세보기 스타일 수정
    related to: #3

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -5,26 +5,29 @@ function ProductItem({ link, src, title, price }: ProductItem) {
     <div className="relative">
       <Link to={link} className="flex flex-col gap-3">
         <figure className="flex flex-col gap-3 justify-center items-center py-3">
-          <div className="w-64 h-64 overflow-hidden">
+          <div className="w-60 h-60 overflow-hidden">
             <img src={src} alt={title} className="hover:scale-110" />
           </div>
           <figcaption className="text-sm">{title}</figcaption>
         </figure>
       </Link>
       <p className="font-semibold">{price}</p>
-      <div className="absolute top-3 right-0 bg-black bg-opacity-60 w-64 h-64">
-        <div className="absolute bottom-6 left-1/2 -translate-x-1/2 bg-white py-2 px-2 w-36">
-          <button type="button" className="pr-3">
+
+      {/* 카트, 찜, 상세보기 */}
+      <div className="absolute top-3 right-0 bg-black bg-opacity-60 w-60 h-60">
+        <div className="absolute bottom-6 left-1/2 -translate-x-1/2 bg-white w-40 flex justify-center py-2 px-2">
+          <button type="button" className="pr-3 flex">
+            <img src="/cart.png" alt="장바구니" className="w-5 h-5" />
             카트
           </button>
           <button
             type="button"
             className="border-l-[1px] border-r-[1px] border-gray-200 px-3"
           >
-            찜
+            <img src="/like.png" alt="찜하기" className="w-5 h-5" />
           </button>
           <button type="button" className="pl-3">
-            확대
+            <img src="/productMore.png" alt="상세보기" className="w-5 h-5" />
           </button>
         </div>
       </div>
