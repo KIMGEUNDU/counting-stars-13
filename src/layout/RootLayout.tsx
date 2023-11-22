09a5bf985af8cce9373454089a1ad1750963a522
HYHYJ <jjangdaram@naver.com>
2023-11-22 16:50:07 +0900

    feat: RootLayout main 태그 추가
    RootLayout main 태그 추가

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -6,7 +6,9 @@ export default function RootLayout() {
   return (
     <>
       <Header />
-      <Outlet />
+      <main className="pt-24">
+        <Outlet />
+      </main>
       <Footer />
     </>
   );
