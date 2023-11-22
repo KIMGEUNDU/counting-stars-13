ba55d845e0d98b3c4c4fd93f78895540de7673dd
rustandbone <dlehdgh14@naver.com>
2023-11-22 16:45:49 +0900

    style: 공지사항 페이지 스타일링
    related to: #4

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -1,40 +1,38 @@
 import PageMainTitle from 'components/PageMainTitle';
+import EachPost from 'components/EachPost';
 
 export default function Community() {
   return (
     <>
-      <main>
+      <main className="min-h-[60vh]">
         <PageMainTitle title="공지사항" />
-        <section className="w-4/5 mx-auto border border-gray-300">
+        <section className="w-4/5 mx-auto border-t-2 border-gray-300">
           <table className="w-full">
-            <thead className="border-b border-gray-300">
-              <th className="py-4 text-center w-20">번호</th>
-              <th>제목</th>
-              <th>작성자</th>
-              <th className="w-24">작성일</th>
+            <thead className="border-b border-gray-300 bg-gray-100">
+              <th className="py-4 text-center w-12 font-normal">번호</th>
+              <th className="font-normal">제목</th>
+              <th className="w-24 font-normal">작성자</th>
+              <th className="w-24 font-normal">작성일</th>
             </thead>
             <tbody className="text-center">
-              <tr className="border-b border-gray-300">
-                <td className="py-4">공지</td>
-                <td>블랙 프라이데이 할인 이벤트</td>
-                <td>윤동주</td>
-                <td>
-                  2023-11-21
-                  <br /> 17:11:18
-                </td>
-              </tr>
-              <tr className="border-b border-gray-300">
-                <td>공지</td>
-                <td>블랙 프라이데이 할인 이벤트</td>
-                <td>윤동주</td>
-                <td>2023-11-21 17:11:18</td>
-              </tr>
-              <tr className="border-b border-gray-300">
-                <td>공지</td>
-                <td>블랙 프라이데이 할인 이벤트</td>
-                <td>윤동주</td>
-                <td>2023-11-21 17:11:18</td>
-              </tr>
+              <EachPost
+                tag="공지"
+                title="이벤트 타이틀"
+                writer="윤동주"
+                date="2013-11-21 17:11:18"
+              />
+              <EachPost
+                tag="공지"
+                title="이벤트 타이틀"
+                writer="윤동주"
+                date="2013-11-21 17:11:18"
+              />
+              <EachPost
+                tag="공지"
+                title="이벤트 타이틀"
+                writer="윤동주"
+                date="2013-11-21 17:11:18"
+              />
             </tbody>
           </table>
         </section>
