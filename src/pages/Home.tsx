07592de66d218add265c309974c57562f4a9e40b
KIMGEUNDU <vlkoklv@naver.com>
2023-11-22 13:17:38 +0900

    docs: 필요 컴포넌트 코드 추가
    related to: #3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import BestItem from '@/components/BestItem';
 import ProductContainer from '@/components/ProductContainer';
 import ReviewSlide from '@/components/ReviewSlide';
 import SlideBanner from '@/components/SlideBanner';
+import Slogan from '@/components/Slogan';
 
 export default function Home() {
   return (
@@ -9,9 +10,7 @@ export default function Home() {
       <SlideBanner />
       <BestItem />
       <ProductContainer />
-      <p className="text-starPink text-8xl font-bold text-center py-28">
-        We Make Pets Fun!
-      </p>
+      <Slogan />
       <ReviewSlide />
     </>
   );
