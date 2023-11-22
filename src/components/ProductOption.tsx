4a60af32ae8ab5360d4b7a5b8aadf1b3a614b986
KIMGEUNDU <vlkoklv@naver.com>
2023-11-22 16:48:46 +0900

    init: ProductOption 컴포넌트 분리 생성
    related to: #17

---
+++ src/components/ProductOption.tsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+function ProductOption() {
+  return (
+    <div className="productOption invisible absolute bottom-16 left-1/2 -translate-x-1/2 bg-white w-40 flex justify-center py-2 px-2">
+      <button type="button" className="pr-3 flex">
+        <img src="/cart.png" alt="장바구니" className="w-5 h-5" />
+        카트
+      </button>
+      <button
+        type="button"
+        className="border-l-[1px] border-r-[1px] border-gray-200 px-3"
+      >
+        <img src="/like.png" alt="찜하기" className="w-5 h-5" />
+      </button>
+      <Link to="/detail">
+        <button type="button" className="pl-3">
+          <img src="/productMore.png" alt="상세보기" className="w-5 h-5" />
+        </button>
+      </Link>
+    </div>
+  );
+}
+
+export default ProductOption;
