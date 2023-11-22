5b3cae65d052883eef659756b445c7f8dcf642f0
KIMGEUNDU <vlkoklv@naver.com>
2023-11-22 16:47:29 +0900

    ts: 타입 추가
    related to: #17

--- src/components/PageMap.tsx
+++ src/components/PageMap.tsx
@@ -1,14 +1,23 @@
 import { Link } from 'react-router-dom';
 
-function PageMap({ route }: { route: string }) {
+function PageMap({
+  route,
+  category = '',
+}: {
+  route: string;
+  category?: string;
+}) {
   return (
     <div className="center py-3 flex gap-2 mt-7">
       <Link to="/">
         <span className="m-auto">홈 &#62;</span>
       </Link>
       <Link to={`/${route}`}>
-        <span className="font-bold">{route}</span>
+        <span className={category ? '' : 'font-bold'}>
+          {route} {category ? '＞' : ''}
+        </span>
       </Link>
+      {category && <span className="font-bold">{category}</span>}
     </div>
   );
 }
