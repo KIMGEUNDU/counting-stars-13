f47ccd963c6b30862794cb4146587f000d336861
rustandbone <dlehdgh14@naver.com>
2023-11-23 15:20:57 +0900

    style: 푸터 스타일 정돈

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,27 +1,24 @@
+import Logo from '@/components/Logo';
 import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
-    <footer className="w-full border-b-2 flex flex-col items-center">
-      <div className="w-[80%] flex justify-between mt-12">
+    <footer className="w-full flex flex-col items-center">
+      <div className="h-16 w-full bg-starPink my-8"></div>
+      <section className="w-4/5 flex justify-evenly">
         <ul className="font-bold">
           <li className="mb-4">
-            <Link to="/brand">BRAND</Link>
+            <Link to="/brand">Brand</Link>
           </li>
           <li className="mb-4 ">
-            <Link to="/shop">SHOP</Link>
+            <Link to="/shop">Shop</Link>
           </li>
           <li className="mb-4">
-            <Link to="/community">COMMUNITY</Link>
+            <Link to="/community">Community</Link>
           </li>
         </ul>
-        <div>
-          <img
-            alt="별해달 로고"
-            src="/logoWord.png"
-            width="92px"
-            className="mb-4"
-          />
+        <div className="-mt-3">
+          <Logo width="w-24 m-0" />
           <ul className="flex flex-col gap-2 text-gray-400">
             <li>(주)별,해달 | 대표 : 윤동주</li>
             <li>
@@ -30,21 +27,21 @@ export default function Footer() {
                 나이로비
               </address>
             </li>
-            <li>팀: 멋쟁이사자처럼 플러스 1기 13조 윤동주</li>
+            <li>팀 : 멋쟁이사자처럼 플러스 1기 13조 윤동주</li>
           </ul>
         </div>
         <button
           aria-label="페이지 맨 위로 올라가기"
-          className="w-20 h-20 bg-starPink text-white font-bold rounded-[20%] "
+          className="w-20 h-20 bg-starPink text-white font-bold rounded-[20%]"
         >
           <img src="/footerArrow.png" className="mx-auto mb-2" />
           GO UP
         </button>
-      </div>
+      </section>
 
-      <div className="text-gray-300 mt-2">
+      <small className="text-gray-500 text-base my-2">
         copyright (c) <strong>별,해달</strong> all rights reserved.
-      </div>
+      </small>
     </footer>
   );
 }
