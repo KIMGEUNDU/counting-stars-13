88b599253e81f6c9d81cbd0242f6da4b7bd8b1f3
KIMGEUNDU <vlkoklv@naver.com>
2023-11-23 14:51:04 +0900

    rename: 페이지별 컴포넌트로 이동
    related to: #3

---
+++ src/components/Home/BestItem.tsx
@@ -0,0 +1,47 @@
+import banner from '/eventBanner.png';
+import { Link } from 'react-router-dom';
+import MainTitle from '../MainTitle';
+import ProductItem from '../Shop/ProductItem';
+
+function BestItem() {
+  return (
+    <div className="w-[950px] m-auto text-center pb-20">
+      <MainTitle title="Best Item" />
+      <div className="flex gap-3 justify-center py-7">
+        <button type="button" className="filterButton bg-zinc-900 text-white">
+          # 특별한날
+        </button>
+        <button type="button" className="filterButton">
+          # 자연식
+        </button>
+        <button type="button" className="filterButton">
+          # 육포
+        </button>
+        <button type="button" className="filterButton">
+          # 천연껌
+        </button>
+      </div>
+      <div className="flex gap-1">
+        <ul className="flex gap-5 flex-wrap">
+          {Array(4)
+            .fill('')
+            .map((_, i) => (
+              <li key={i}>
+                <ProductItem
+                  link="#"
+                  src="https://ggaggamukja.com/web/product/big/202303/9a0d91db522758c9ba7e3a764acca74e.jpg"
+                  title="멍돈까스"
+                  price="8,000"
+                />
+              </li>
+            ))}
+        </ul>
+        <Link to="/join" className="w-[600px]">
+          <img src={banner} alt="이벤트배너" className="w-full" />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default BestItem;
