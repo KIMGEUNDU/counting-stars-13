1b0f974e5482b3c61f7ab5604d2752dccb449013
KIMGEUNDU <vlkoklv@naver.com>
2023-11-23 14:40:07 +0900

    rename: 페이지별 폴더로 이동

--- src/components/BrandIntroduce.tsx
+++
@@ -1,5 +0,0 @@
-function BrandIntroduce({ title }: { title: string }) {
-  return <p className="text-4xl font-bold">{title}</p>;
-}
-
-export default BrandIntroduce;
--- src/components/Logo.tsx
+++
@@ -1,7 +0,0 @@
-function Logo({ width }: { width: string }) {
-  return (
-    <img className={`${width} m-auto`} src="/logoWord.png" alt="별해달로고" />
-  );
-}
-
-export default Logo;
