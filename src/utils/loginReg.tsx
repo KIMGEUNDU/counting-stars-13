917a99b7fa883de29fac42865aeff9107d3fd152
HYHYJ <jjangdaram@naver.com>
2023-11-27 17:31:59 +0900

    feat: Join페이지 기능추가
    1. 회원가입 값 저장
    2. 유효성 검사
    3. loginReg.tsx 유효성 검사 파일에 이메일, 핸드폰번호 유효성검사 기능 추가
    
    fixes: #45

--- src/utils/loginReg.tsx
+++ src/utils/loginReg.tsx
@@ -1,5 +1,5 @@
 //아이디: 영문자로 시작하는 영문자 또는 숫자 4~16자
-export function emailReg(text: string) {
+export function idReg(text: string) {
   const re = /^[a-z]+[a-z0-9]{3,15}$/g;
 
   return re.test(String(text).toLowerCase());
@@ -7,5 +7,18 @@ export function emailReg(text: string) {
 //비밀번호: 영문, 숫자 조합으로 8~16자
 export function pwReg(text: string) {
   const re = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/;
+  return re.test(String(text).toLowerCase());
+}
+//이메일: 영문자로 시작하는 영문자 또는 숫자 4~16자
+export function emailReg(text: string) {
+  const re =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i; //이메일 정규식
+
+  return re.test(String(text).toLowerCase());
+}
+//핸드폰 번호 유효성 검사
+export function phoneReg(text: string) {
+  const re = /^\d{3}-\d{3,4}-\d{4}$/;
+
   return re.test(String(text).toLowerCase());
 }
