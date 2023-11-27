0196c2c61ddb96ffe109711b3844013961dd7601
KIMGEUNDU <vlkoklv@naver.com>
2023-11-27 17:28:37 +0900

    docs: 업로드 코드 추가
    related to: #38

--- src/pages/WriteQna.tsx
+++ src/pages/WriteQna.tsx
@@ -2,16 +2,23 @@ import PageMainTitle from '@/components/PageMainTitle';
 import FormAttachFile from '@/components/QnA,Review/FormAttachFile';
 import FormCkEditor from '@/components/QnA,Review/FormCkEditor';
 import FormTitleInput from '@/components/QnA,Review/FormTitleInput';
+import Modal from '@/components/QnA,Review/Modal';
 import ProductSelect from '@/components/QnA,Review/ProductSelect';
 import WriteButton from '@/components/QnA,Review/WriteButton';
+import { useData } from '@/store/useData';
 import { useForm } from '@/store/useForm';
 import axios from 'axios';
+import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 export default function WriteQna() {
   const { title, content, attachFile } = useForm();
+  const { modal, setModal, selectData, setSelectId, setSelectData } = useData();
+  const navigate = useNavigate();
 
+  // Qna 등록하기
   const handleRegistQna = async (e: React.FormEvent) => {
     e.preventDefault();
 
@@ -26,16 +33,38 @@ export default function WriteQna() {
       title,
       content,
       attachFile,
+      selectData,
     };
 
-    try {
-      // {ok: 0, message: 'authorization 헤더가 없습니다.'}
-      // 아마 로그인안해서 에러나는 듯?
-      await axios.post('https://localhost/api/replies', newQna);
-    } catch (err) {
-      console.log(err);
+    // {ok: 0, message: 'authorization 헤더가 없습니다.'}
+    // 아마 로그인안해서 에러나는 듯?
+    // replies는 구매후기이니 카테고리 생기면 변경
+    const response = await axios.post('https://localhost/api/replies', newQna);
+
+    if (response.data.ok === 1) {
+      toast('업로드하였습니다 :)', {
+        icon: '⭐',
+        duration: 2000,
+      });
+
+      navigate(`/qna-detail/${response.data.id}`);
     }
   };
+
+  // Esc키로 모달창 닫기
+  if (modal) {
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        setModal(!modal);
+      }
+    });
+  }
+
+  // 새로 Qna 페이지 들어올때는 리셋, 왜 안돼?
+  useEffect(() => {
+    setSelectId(0);
+    setSelectData(null);
+  }, [setSelectData, setSelectId]);
 
   return (
     <>
@@ -46,7 +75,8 @@ export default function WriteQna() {
       <main>
         <PageMainTitle title="상품 Q&A" />
         <form className="w-4/5 mx-auto" onSubmit={handleRegistQna}>
-          <ProductSelect title="상품 선택" />
+          <ProductSelect title="상품 선택" onClick={() => setModal(!modal)} />
+          {modal && <Modal onClick={() => setModal(!modal)} />}
           <table className="w-full border-t border-gray-300">
             <tbody>
               <FormTitleInput />
