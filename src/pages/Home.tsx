21bfd1f82ae1e74c340031331c551c00a68714cb
rustandbone <dlehdgh14@naver.com>
2023-11-27 17:26:06 +0900

    feat:  메인 페이지 전체 목록 불러오기
    related to: #46

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,31 @@ import ProductContainer from '@/components/Shop/ProductContainer';
 import ReviewSlide from '@/components/Home/ReviewSlide';
 import SlideBanner from '@/components/Home/SlideBanner';
 import Slogan from '@/components/Brand/Slogan';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+
+axios.defaults.baseURL = 'https://localhost/api';
 
 export default function Home() {
+  const [productData, setProductData] = useState([]);
+  const { isLoading, data, error } = useQuery({
+    queryKey: ['products'],
+    queryFn: () => axios.get(`/products`),
+    select: (data) => data.data.item,
+    staleTime: 1000 * 2,
+    // refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    setProductData(data);
+  }, [isLoading, data, error]);
+
   return (
     <>
       <SlideBanner />
       <BestItem />
-      <ProductContainer />
+      <ProductContainer data={productData} />
       <Slogan />
       <ReviewSlide />
     </>
