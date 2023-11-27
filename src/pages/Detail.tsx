aa67795aa5696d0b369e994c33976de50d70e4f4
rustandbone <dlehdgh14@naver.com>
2023-11-27 17:33:02 +0900

    feat: 상세 페이지 해당 물품 정보 가져오기 및 전달

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -2,19 +2,31 @@ import DetailImg from '@/components/Detail/DetailImg';
 import DetailProductInformation from '@/components/Detail/DetailProductInformation';
 import DetailProductOption from '@/components/Detail/DetailProductOption';
 import PageMap from '@/components/PageMap';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://localhost/api';
 
 function Detail() {
+  const { id } = useParams();
+
+  const { data } = useQuery({
+    queryKey: ['products', id],
+    queryFn: () => axios.get(`/products/${id}`),
+    select: (data) => data.data.item,
+    staleTime: 1000 * 2,
+    refetchOnWindowFocus: false,
+  });
+
   return (
     <div>
       <PageMap route="SHOP" />
       <div className="center flex justify-between py-10 border-b-[1px] border-b-gray-300 mb-20">
-        <DetailImg
-          src="https://ggaggamukja.com/web/product/extra/big/202207/f9b60ded23b1d7c266b16fff24a03a01.jpg"
-          alt="상품제목"
-        />
-        <DetailProductOption />
+        <DetailImg src={data?.mainImages[0]} alt={data?.name} />
+        <DetailProductOption data={data} />
       </div>
-      <DetailProductInformation />
+      <DetailProductInformation imgArray={data?.descriptImages} />
     </div>
   );
 }
