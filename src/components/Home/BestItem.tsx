8e2fbcd4fbadcd8b0eea8fa6ccfa26c7941faf81
rustandbone <dlehdgh14@naver.com>
2023-11-27 17:35:53 +0900

    feat: 메인 페이지 - Best Item 렌더링 및 적용

--- src/components/Home/BestItem.tsx
+++ src/components/Home/BestItem.tsx
@@ -2,45 +2,89 @@ import banner from '/eventBanner.png';
 import { Link } from 'react-router-dom';
 import MainTitle from '../MainTitle';
 import ProductItem from '../Shop/ProductItem';
+import axios from 'axios';
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 function BestItem() {
+  const [category, setCategory] = useState('party');
+
+  const { data } = useQuery({
+    queryKey: ['products', category],
+    queryFn: () =>
+      axios.get(`/products?`, {
+        params: {
+          extra: JSON.stringify({
+            'extra.category.0': `PC-shop`,
+            'extra.category.1': `PC-${category}`,
+          }),
+        },
+      }),
+    select: (data) => data.data.item,
+    staleTime: 1000 * 2,
+    refetchOnWindowFocus: false,
+  });
+
+  const handleClick = (tag: string) => {
+    setCategory(tag);
+  };
+
   return (
-    <div className="w-[950px] m-auto text-center pb-20">
+    <section className="w-4/5 m-auto text-center pb-20">
       <MainTitle title="Best Item" />
-      <div className="flex gap-3 justify-center py-7">
-        <button type="button" className="filterButton bg-zinc-900 text-white">
-          # 특별한날
+
+      <article className="flex gap-3 justify-center py-7">
+        <button
+          type="button"
+          onClick={() => handleClick('party')}
+          className="filterButton bg-zinc-900 text-white"
+        >
+          #특별한_날
         </button>
-        <button type="button" className="filterButton">
-          # 자연식
+        <button
+          type="button"
+          onClick={() => handleClick('special')}
+          className="filterButton"
+        >
+          #자연식
         </button>
-        <button type="button" className="filterButton">
-          # 육포
+        <button
+          type="button"
+          onClick={() => handleClick('gum')}
+          className="filterButton"
+        >
+          #육포
         </button>
-        <button type="button" className="filterButton">
-          # 천연껌
+        <button
+          type="button"
+          onClick={() => handleClick('bone')}
+          className="filterButton"
+        >
+          #천연껌
         </button>
-      </div>
-      <div className="flex gap-1">
-        <ul className="flex gap-5 flex-wrap">
-          {Array(4)
-            .fill('')
-            .map((_, i) => (
-              <li key={i}>
-                <ProductItem
-                  link="#"
-                  src="https://ggaggamukja.com/web/product/big/202303/9a0d91db522758c9ba7e3a764acca74e.jpg"
-                  title="멍돈까스"
-                  price="8,000"
-                />
-              </li>
-            ))}
+      </article>
+
+      <article className="flex gap-1">
+        <ul className="flex gap-5 flex-wrap min-w-[500px]">
+          {data &&
+            data.slice(0, 8).map((item: any) => {
+              return (
+                <li key={item._id}>
+                  <ProductItem
+                    link={item._id}
+                    src={item.mainImages[0]}
+                    title={item.name}
+                    price={item.price}
+                  />
+                </li>
+              );
+            })}
         </ul>
-        <Link to="/join" className="w-[600px]">
-          <img src={banner} alt="이벤트배너" className="w-full" />
+        <Link to="/join" className="w-3/5 ml-auto">
+          <img src={banner} alt="이벤트 배너" className="w-full" />
         </Link>
-      </div>
-    </div>
+      </article>
+    </section>
   );
 }
 
