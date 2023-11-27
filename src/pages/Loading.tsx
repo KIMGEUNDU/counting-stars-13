ea5abbf5d8cff6c641c6e5873592498961c4ea61
KIMGEUNDU <vlkoklv@naver.com>
2023-11-27 17:24:38 +0900

    init: 로딩 컴포넌트 생성

---
+++ src/pages/Loading.tsx
@@ -0,0 +1,10 @@
+function Loading() {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <img src="/logoChar.png" alt="로고캐릭터" className="w-96" />
+      <p className="text-3xl">열심히 불러오는중...</p>
+    </div>
+  );
+}
+
+export default Loading;
