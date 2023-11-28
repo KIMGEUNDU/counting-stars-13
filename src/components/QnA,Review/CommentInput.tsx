cba42d2582dc798cfb39cd3567229ca6f266baaf
KIMGEUNDU <vlkoklv@naver.com>
2023-11-28 11:22:39 +0900

    docs: 임시 댓글 기능 코드 추가

--- src/components/QnA,Review/CommentInput.tsx
+++ src/components/QnA,Review/CommentInput.tsx
@@ -1,13 +1,37 @@
+import { useComment } from '@/store/useComment';
+import { writeDate } from '@/utils/writeDate';
+import { FormEvent, useRef } from 'react';
+
 function CommentInput({ writer, pw }: { writer: string; pw: string }) {
+  const { setQna } = useComment();
+  const commentRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const uploadComment = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (commentRef.current && commentRef.current.value) {
+      const comment = {
+        writer,
+        content: commentRef.current.value,
+        date: writeDate(),
+      };
+
+      setQna(comment);
+    }
+  };
+
   return (
-    <form className="center border border-gray-300 p-3 bg-gray-50 text-sm flex flex-col gap-3 my-7">
+    <form
+      className="center border border-gray-300 p-3 bg-gray-50 text-sm flex flex-col gap-3 my-7"
+      onSubmit={uploadComment}
+    >
       <h3 className="font-semibold">댓글달기</h3>
       <fieldset>
         <label htmlFor="name">이름 :</label>
         <input
           id="name"
           type="text"
-          value={writer}
+          defaultValue={writer}
           className="border border-gray-300 ml-3 px-1 w-1/6"
           required
         />
@@ -17,22 +41,23 @@ function CommentInput({ writer, pw }: { writer: string; pw: string }) {
         <input
           id="pw"
           type="password"
-          value={pw}
+          defaultValue={pw}
           className="border border-gray-300 ml-3 px-1 w-1/6"
           required
         />
       </fieldset>
       <fieldset className="flex">
-        <label htmlFor="content" className="hidden">
+        <label htmlFor="comment" className="hidden">
           댓글입력창
         </label>
         <textarea
-          name="content"
-          id="content"
+          name="comment"
+          id="comment"
           cols={30}
           rows={10}
           className="w-full h-12 border border-gray-300 mr-3"
           required
+          ref={commentRef}
         />
         <button
           type="submit"
