5a47bff9d86ee8d1008aeb64d73e8eb6897a12b2
rustandbone <dlehdgh14@naver.com>
2023-11-28 14:47:35 +0900

    fix: defaultValue로 수정 및 버튼 타입 추가

--- src/components/Detail/ProductQuantity.tsx
+++ src/components/Detail/ProductQuantity.tsx
@@ -1,12 +1,12 @@
 function ProductQuantity({ value }: { value: number }) {
   return (
     <div className="flex border-2 h-9 rounded-lg justify-around mb-2">
-      <input type="text" className="w-9 pl-1" value={value} />
+      <input type="text" className="w-9 pl-1" defaultValue={value} />
       <div className="flex flex-col gap-2 justify-center">
-        <button>
+        <button type="button">
           <img src="/cartArrowUp.png" className="w-3" />
         </button>
-        <button>
+        <button type="button">
           <img src="/cartArrowDown.png" className="w-3" />
         </button>
       </div>
