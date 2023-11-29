58efc9a549cabdd8d0e1734a361190b5cb43b186
KIMGEUNDU <vlkoklv@naver.com>
2023-11-29 17:30:46 +0900

    docs: 더미데이터 생성 후 가져오기

--- src/pages/Qna.tsx
+++ src/pages/Qna.tsx
@@ -1,12 +1,19 @@
-import PageMainTitle from 'components/PageMainTitle';
-import EachPost from 'components/EachPost';
-import { Helmet } from 'react-helmet-async';
+import PaginationNumber from '@/components/PaginationNumber';
 import Thead from '@/components/QnA,Review/Thead';
 import WriterButton from '@/components/QnA,Review/WriterButton';
+import { dummyData } from '@/store/dummyData';
+import { sortQnaReviewData } from '@/utils/getProductsData';
+import EachPost from 'components/EachPost';
+import PageMainTitle from 'components/PageMainTitle';
+import { Helmet } from 'react-helmet-async';
 
 export default function Qna() {
   // 현재 qna 컬렉션없음
+  // 임시데이터
+  const { qnaData } = dummyData();
 
+  // id순으로 정렬하기
+  const sortQnaData = sortQnaReviewData(qnaData);
   return (
     <>
       <Helmet>
@@ -15,25 +22,25 @@ export default function Qna() {
 
       <main className="min-h-[60vh]">
         <PageMainTitle title="상품 Q&A" />
-        <section className="w-4/5 mx-auto border-t-2 border-gray-300">
+        <section className="w-4/5 mx-auto border-t-2 border-gray-300 relative">
           <table className="w-full">
             <Thead info="상품 정보" />
             <tbody className="text-center">
-              {Array(5)
-                .fill(0)
-                .map((_, i) => (
+              {sortQnaData &&
+                sortQnaData.map((v, i) => (
                   <EachPost
                     key={i}
-                    tag={i + 1}
-                    title="이벤트 타이틀"
-                    writer="윤동주"
-                    date="2023-11-21 17:11:18"
-                    item="까까묵자 생일 파티 세트"
-                    link="/qna-detail"
+                    tag={v._id}
+                    title={v.title}
+                    writer={v.writer}
+                    date={v.date}
+                    item={v.productName}
+                    link={`/qna-detail/${v._id}`}
                   />
                 ))}
             </tbody>
           </table>
+          <PaginationNumber length={Math.ceil(qnaData.length / 10)} />
           <WriterButton link="/write-qna" />
         </section>
       </main>
