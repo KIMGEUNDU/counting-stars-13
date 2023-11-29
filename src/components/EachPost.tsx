d9ec5248a799cabc09a5d127c95623414da3d434
KIMGEUNDU <vlkoklv@naver.com>
2023-11-29 17:26:43 +0900

    docs: 조건문 추가

--- src/components/EachPost.tsx
+++ src/components/EachPost.tsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 
 type board = {
-  tag: number | string;
+  tag: number | string | undefined;
   title: string;
   writer: string;
   link?: string;
   date: string;
   item?: string;
-  grade?: string;
+  grade?: number;
 };
 
 export default function EachPost({
@@ -20,23 +20,37 @@ export default function EachPost({
   link,
 }: board) {
   const writerPrivate = (writer: string) => {
-    return writer.substring(0, 1) + '*'.repeat(`${writer}`.length - 1);
+    if (writer !== '별해달') {
+      return writer.substring(0, 1) + '*'.repeat(`${writer}`.length - 1);
+    } else {
+      return '별해달';
+    }
+  };
+
+  const starGrade = (grade: number) => {
+    return '⭐'.repeat(grade);
   };
 
   return (
     <>
       <tr className="border-b border-gray-200">
-        <td className="py-4">{tag}</td>
-        {item && <td className="w-1/4">{item}</td>}
-        {grade && <td>{grade}</td>}
+        <td className="py-4 w-1/12">{tag}</td>
+        {item && <td className=" whitespace-nowrap w-[12%]">{item}</td>}
+        {grade && <td className="truncate w-[12%]">{starGrade(grade)}</td>}
         {link && (
-          <td className="text-left">
-            <Link to={link}>{title}</Link>
+          <td className="text-left w-1/2">
+            <Link to={link} className="line-clamp-1">
+              {title}
+            </Link>
           </td>
         )}
-        {!link && <td className="text-left">{title}</td>}
-        <td>{writerPrivate(writer)}</td>
-        <td className="font-extralight w-1/6">{date}</td>
+        {!link && (
+          <td className="text-left">
+            <span className="">{title}</span>
+          </td>
+        )}
+        <td className="w-[10%]">{writerPrivate(writer)}</td>
+        <td className="font-extralight w-[10%]">{date}</td>
       </tr>
     </>
   );
