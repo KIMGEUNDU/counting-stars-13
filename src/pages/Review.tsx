3dd195af53cddbd84f7064dbb1c38dfef40d202a
KIMGEUNDU <vlkoklv@naver.com>
2023-11-30 17:28:46 +0900

    docs: 전역 상태로 관리하는 코드 추가중

--- src/pages/Review.tsx
+++ src/pages/Review.tsx
@@ -2,18 +2,47 @@ import PaginationNumber from '@/components/PaginationNumber';
 import Thead from '@/components/QnA,Review/Thead';
 import WriterButton from '@/components/QnA,Review/WriterButton';
 import { dummyData } from '@/store/dummyData';
+import { useData } from '@/store/useData';
+import { useForm } from '@/store/useForm';
 import { sortQnaReviewData } from '@/utils/getProductsData';
 import EachPost from 'components/EachPost';
 import PageMainTitle from 'components/PageMainTitle';
+import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 
 export default function Review() {
+  const {
+    data,
+    setData,
+    setPageData,
+    setSelectId,
+    setSelectData,
+    setSelectOrderId,
+  } = useData();
+  const { setAttachFile } = useForm();
   // 현재 후기 조회안됨 -> 곧 API 구현 예정
   // 더미데이터 가지고오기
   const { reviewData } = dummyData();
 
   // id순으로 정렬하기
   const sortReviewData = sortQnaReviewData(reviewData);
+
+  // 새로 Review 페이지 들어올때는 리셋
+  useEffect(() => {
+    setSelectId(null);
+    setSelectData(null);
+    setSelectOrderId(null);
+    setAttachFile('');
+  }, []);
+
+  // 실험중
+  // useEffect(() => {
+  //   setData(sortReviewData);
+  //   setPageData(data.slice(0, 10));
+  // }, [setData]);
+
+  // console.log('리뷰페이지 ');
+  // console.log(data);
 
   return (
     <>
@@ -30,11 +59,12 @@ export default function Review() {
               {sortReviewData.map((v, i) => (
                 <EachPost
                   key={i}
-                  tag={v._id}
+                  tag={v._id ? v._id : ''}
                   title={v.title}
                   writer={v.writer}
                   date={v.date}
                   item={v.productName}
+                  itemImg={v.productImg}
                   grade={v.grade}
                   link={`/review-detail/${v._id}`}
                 />
