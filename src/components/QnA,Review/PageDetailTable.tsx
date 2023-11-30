690b3ab3789c56dfe9edda60648aace03e1338cc
KIMGEUNDU <vlkoklv@naver.com>
2023-11-30 17:28:13 +0900

    style: 스타일 수정

--- src/components/QnA,Review/PageDetailTable.tsx
+++ src/components/QnA,Review/PageDetailTable.tsx
@@ -3,9 +3,9 @@ function PageDetailTable({
   writer,
   content,
   date,
-  view,
   grade,
-}: QnaReviewTable) {
+  attachFile,
+}: QnaReviewData) {
   const writerPrivate = (writer: string) => {
     return writer.substring(0, 1) + '*'.repeat(`${writer}`.length - 1);
   };
@@ -15,11 +15,11 @@ function PageDetailTable({
       <table className="QnaReviewTable w-full border-t-2 border-t-gray-500 border-b border-b-gray-300 text-left">
         <tbody>
           <tr>
-            <td>제목</td>
+            <td className="w-1/12 whitespace-nowrap">제목</td>
             <td>{title}</td>
           </tr>
           <tr>
-            <td>작성자</td>
+            <td className="w-1/12 whitespace-nowrap">작성자</td>
             <td>{writerPrivate(writer)}</td>
           </tr>
         </tbody>
@@ -27,15 +27,13 @@ function PageDetailTable({
       <ul className="flex border-b border-b-gray-200 py-5 px-2">
         {date && (
           <>
-            <li className="font-semibold pr-3">작성일</li>
+            <li className="font-semibold pr-3 whitespace-nowrap">작성일</li>
             <li className="text-gray-400 pr-10">{date}</li>
-            <li className="font-semibold pr-3">조회수</li>
-            <li className="text-gray-400">{view}</li>
           </>
         )}
         {grade && (
           <>
-            <li className="pr-2 font-semibold">평점</li>
+            <li className="pr-2 font-semibold whitespace-nowrap">평점</li>
             <li>
               {Array(grade)
                 .fill('★')
@@ -55,6 +53,11 @@ function PageDetailTable({
           </>
         )}
       </ul>
+      {attachFile && (
+        <p className="py-5">
+          <img className="w-48 h-auto m-auto" src={attachFile} alt="첨부파일" />
+        </p>
+      )}
       <p className="py-10 px-2 border-b border-b-gray-200">{content}</p>
     </div>
   );
