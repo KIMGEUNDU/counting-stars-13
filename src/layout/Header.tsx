f26a46f4701bf3f5142599a2d5c3ca64196ad76a
KIMGEUNDU <vlkoklv@naver.com>
2023-12-01 17:38:27 +0900

    docs: Header 반응형으로 수정

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -17,6 +17,7 @@ export default function Header() {
   useEffect(() => {
     localStorage.getItem('token') ? setLogin(true) : setLogin(false);
   }, []);
+
   const navigate = useNavigate();
   const handleLogout = () => {
     localStorage.clear();
@@ -34,82 +35,83 @@ export default function Header() {
       <div className="w-full border-b-2 flex justify-center fixed h-[96px] bg-white">
         <div className="center">
           <h1 className="sr-only">별해달</h1>
-          <nav className="flex gap-9 justify-between">
-            <ul className="w-4/10 flex items-center gap-14 font-bold">
-              <li className="px-2 py-5">
-                <Link to="/brand">Brand</Link>
-              </li>
-              <li className="group/item  px-2 py-5 relative">
-                <Link to="/shop/shop">Shop</Link>
-                <div className="group/edit group-hover/item:visible  invisible  absolute bg-white rounded-md py-1 border w-36 text-center text-sm font-medium left-[-40px] mt-2">
-                  <Link to="/shop/dessert" className="block py-1">
-                    디저트/케이크
-                  </Link>
-                  <Link to="/shop/special" className="block py-1">
-                    자연식/특식
-                  </Link>
-                  <Link to="/shop/gum" className="block py-1">
-                    육포/우유껌
-                  </Link>
-                  <Link to="/shop/bone" className="block py-1">
-                    천연껌/뼈간식
-                  </Link>
-                  <Link to="/shop/party" className="block py-1">
-                    파티용품/굿즈
-                  </Link>
-                </div>
-              </li>
-
-              <li className="group/item  px-2 py-5 relative">
-                Community
-                <div className=" group/edit group-hover/item:visible  invisible  absolute bg-white rounded-md py-1 border w-32 text-center text-sm font-medium  left-[-13px] mt-2">
-                  <Link to="/qna" className="block py-1">
-                    Q&A
-                  </Link>
-                  <Link to="/review" className="block py-1">
-                    Review
-                  </Link>
-                </div>
-              </li>
-            </ul>
+          <nav className="flex justify-between">
+            <div className="w-[45%] flex items-center">
+              <ul className="w-[60%] flex items-center justify-between font-bold">
+                <li className="px-2 py-5">
+                  <Link to="/brand">Brand</Link>
+                </li>
+                <li className="group/item px-2 py-5 relative">
+                  <Link to="/shop/shop">Shop</Link>
+                  <div className="group/edit group-hover/item:visible  invisible  absolute bg-white rounded-md py-1 border w-36 text-center text-sm font-medium left-[-40px] mt-2">
+                    <Link to="/shop/dessert" className="block py-1">
+                      디저트/케이크
+                    </Link>
+                    <Link to="/shop/special" className="block py-1">
+                      자연식/특식
+                    </Link>
+                    <Link to="/shop/gum" className="block py-1">
+                      육포/우유껌
+                    </Link>
+                    <Link to="/shop/bone" className="block py-1">
+                      천연껌/뼈간식
+                    </Link>
+                    <Link to="/shop/party" className="block py-1">
+                      파티용품/굿즈
+                    </Link>
+                  </div>
+                </li>
+                <li className="group/item px-2 py-5 relative">
+                  Community
+                  <div className=" group/edit group-hover/item:visible  invisible  absolute bg-white rounded-md py-1 border w-32 text-center text-sm font-medium  left-[-13px] mt-2">
+                    <Link to="/qna" className="block py-1">
+                      Q&A
+                    </Link>
+                    <Link to="/review" className="block py-1">
+                      Review
+                    </Link>
+                  </div>
+                </li>
+              </ul>
+            </div>
             <Link to="/" aria-label="홈페이지로 가기">
-              <img src="/logoChar.png" className="min-w-[90px] w-[90px]" />
+              <img src="/logoChar.png" className="max-w-[90px]" />
             </Link>
-            <div className="flex gap-4 relative items-center">
-              <ul className="flex w-4/10 items-center gap-5">
-                <li className="min-w-[47px]">
+            <div className="w-[45%] flex gap-1 relative justify-end items-center">
+              <ul className="w-[70%] flex items-center justify-between gap-1 text-[1vw]">
+                <li>
                   {isLoginState ? (
                     <Link
                       to="/"
                       onClick={handleLogout}
-                      className="min-w-[47px]"
+                      className="whitespace-nowrap"
                     >
                       로그아웃
                     </Link>
                   ) : (
-                    <Link to="/login" className="min-w-[45px]">
+                    <Link to="/login" className="whitespace-nowrap">
                       로그인
                     </Link>
                   )}
                 </li>
-                <li className="min-w-[64px]">
+                <li>
                   {isLoginState ? (
-                    <Link to="/edit" className="min-w-[64px]">
+                    <Link to="/edit" className="whitespace-nowrap">
                       회원정보조회
                     </Link>
                   ) : (
-                    <Link to="/join" className="min-w-[47px]">
+                    <Link to="/join" className="whitespace-nowrap">
                       회원가입
                     </Link>
                   )}
                 </li>
-                <li className="min-w-[45px]">
+                <li className="whitespace-nowrap">
                   <Link to="/myCart">장바구니</Link>
                 </li>
-                <li className="min-w-[45px]">
+                <li className="whitespace-nowrap">
                   <Link to="/myOrder">주문조회</Link>
                 </li>
-                <li className="min-w-[49px]">
+                <li className="whitespace-nowrap">
                   <Link to="/myShopping">+마이쇼핑</Link>
                 </li>
               </ul>
@@ -118,10 +120,7 @@ export default function Header() {
                 aria-label="검색 버튼"
                 className="inline-block"
               >
-                <img
-                  src="/headerSearch.png"
-                  className="min-w-[20px] w-[20px]"
-                />
+                <img src="/headerSearch.png" className="max-w-[20px]" />
               </Link>
             </div>
           </nav>
