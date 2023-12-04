23349d3accfd327e50f195003eb6fdbf861fb68f
rustandbone <dlehdgh14@naver.com>
2023-12-04 17:31:41 +0900

    feat: 쇼핑 페이지 정렬 구현 - 이름, 금액, 최신 순
    related to: #89

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -5,14 +5,13 @@ import ProductItem from '@/components/Shop/ProductItem';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet-async';
 import { useParams } from 'react-router-dom';
 
 axios.defaults.baseURL = 'https://localhost/api';
 
 export default function Shop() {
   const { id } = useParams();
-  const [category, setCategory] = useState('');
-
   const { isLoading, data, error } = useQuery({
     queryKey: ['products', id],
     queryFn: () =>
@@ -28,6 +27,8 @@ export default function Shop() {
     staleTime: 1000 * 2,
     refetchOnWindowFocus: false,
   });
+  const [category, setCategory] = useState('');
+  const [arrData, setData] = useState<ProductData[]>();
 
   useEffect(() => {
     switch (id) {
@@ -47,10 +48,44 @@ export default function Shop() {
         setCategory('파티용품/굿즈');
         break;
     }
-  }, [category, id, isLoading, error]);
+    setData(data);
+  }, [category, id, isLoading, error, data]);
+
+  const handlePrice = (sort: string) => {
+    if (sort === 'basic') {
+      setData(data);
+    }
+    if (sort === 'new') {
+      setData(
+        data.toSorted((a: ProductData, b: ProductData) =>
+          a.updatedAt > b.updatedAt ? 1 : -1
+        )
+      );
+    }
+    if (sort === 'name') {
+      setData(
+        data.toSorted((a: ProductData, b: ProductData) =>
+          a.name > b.name ? 1 : -1
+        )
+      );
+    }
+    if (sort === 'high') {
+      setData(
+        data.toSorted((a: ProductData, b: ProductData) => b.price - a.price)
+      );
+    }
+    if (sort === 'low') {
+      setData(
+        data.toSorted((a: ProductData, b: ProductData) => a.price - b.price)
+      );
+    }
+  };
 
   return (
     <>
+      <Helmet>
+        <title>{category} - 별,해달</title>
+      </Helmet>
       <PageMap route="Shop" category={category} />
       <PageMainTitle title={category} />
       <div className="center pb-10">
@@ -58,27 +93,37 @@ export default function Shop() {
           총 <span className="text-lg">{data?.length}</span>개
         </p>
         <ul className="flex gap-7 border-t border-t-gray-400 border-b border-b-gray-300 py-3">
-          <li className="text-gray-600 hover:text-black">
-            <button type="button">신상품</button>
+          <li className="text-gray-600 hover:font-bold">
+            <button type="button" onClick={() => handlePrice('basic')}>
+              기본
+            </button>
           </li>
-          <li className="text-gray-600 hover:text-black">
-            <button type="button">상품명</button>
+          <li className="text-gray-600 hover:font-bold">
+            <button type="button" onClick={() => handlePrice('new')}>
+              신상품
+            </button>
           </li>
-          <li className="text-gray-600 hover:text-black">
-            <button type="button">낮은 가격</button>
+          <li className="text-gray-600 hover:font-bold">
+            <button type="button" onClick={() => handlePrice('name')}>
+              상품명
+            </button>
           </li>
-          <li className="text-gray-600 hover:text-black">
-            <button type="button">높은 가격</button>
+          <li className="text-gray-600 hover:font-bold">
+            <button type="button" onClick={() => handlePrice('low')}>
+              낮은 가격
+            </button>
           </li>
-          <li className="text-gray-600 hover:text-black">
-            <button type="button">사용 후기</button>
+          <li className="text-gray-600 hover:font-bold">
+            <button type="button" onClick={() => handlePrice('high')}>
+              높은 가격
+            </button>
           </li>
         </ul>
-        <ul className="flex gap-4 flex-wrap text-center py-3">
-          {data &&
-            data.map((item: ProductData) => {
+        <ul className="flex flex-wrap text-center py-3">
+          {arrData &&
+            arrData.map((item: ProductData) => {
               return (
-                <li key={item._id}>
+                <li key={item._id} className="w-1/4">
                   <ProductItem
                     link={`${item._id}`}
                     src={item.mainImages[0]}
