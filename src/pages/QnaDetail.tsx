6b26f039632ebd7df98b4163a418f56f70dc726f
KIMGEUNDU <vlkoklv@naver.com>
2023-12-05 17:28:49 +0900

    docs: 강사님 api로 수정

--- src/pages/QnaDetail.tsx
+++ src/pages/QnaDetail.tsx
@@ -6,46 +6,37 @@ import PageDetailTable from '@/components/QnA,Review/PageDetailTable';
 import PageDetailTitle from '@/components/QnA,Review/PageDetailTitle';
 import PageListOrder from '@/components/QnA,Review/PageListOrder';
 import ReviewProductItem from '@/components/QnA,Review/ReviewProductItem';
-import { dummyData } from '@/store/dummyData';
 import { useComment } from '@/store/useComment';
 import { useUserInfo } from '@/store/useUserInfo';
 import { AUTH_ID, AUTH_TOKEN } from '@/utils/AUTH_TOKEN';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import toast from 'react-hot-toast';
 import { useNavigate, useParams } from 'react-router-dom';
 
 function QnaDetail() {
   const navigate = useNavigate();
-  const { qnaData, deleteQnaData } = dummyData();
+  const [data, setData] = useState<Replies[]>([]);
+  const [prevData, setPrevData] = useState<Replies | null>(null);
+  const [currentData, setCurrentData] = useState<Replies | null>(null);
+  const [nextData, setNextData] = useState<Replies | null>(null);
   const { id } = useParams();
   const dataId = Number(id) - 1;
-  const length = qnaData.length;
-  const current = qnaData[dataId];
-  const prev = qnaData[dataId - 1];
-  const next = qnaData[dataId + 1];
+
   // 로그인유저정보
   const { userInfo, setUserInfo } = useUserInfo();
+
   // 임시 qna댓글값
   const { qna } = useComment();
   const idFilterComment = qna.filter((v) => v.qnaId === Number(id));
 
   // 삭제이벤트
   const handleDelete = () => {
-    const answer = confirm('정말 삭제하시겠습니까?');
-    const deleteReview = qnaData.filter((v) => v._id !== Number(id));
-
-    if (answer) {
-      deleteQnaData(deleteReview);
-
-      toast('삭제되었습니다.', {
-        icon: '⭐',
-        duration: 2000,
-      });
-
-      navigate('/qna');
-    }
+    toast('기능 수정중.', {
+      icon: '⭐',
+      duration: 2000,
+    });
   };
 
   // 로그인유저정보 받아오기
@@ -63,40 +54,75 @@ function QnaDetail() {
     getUsers();
   }, [setUserInfo]);
 
+  useEffect(() => {
+    // 현재 데이터
+    const repliesCurrentData = async () => {
+      const res = await axios.get(`https://localhost/api/replies/${id}`, {
+        headers: {
+          Authorization: `Bearer ${AUTH_TOKEN()}`,
+        },
+      });
+
+      setCurrentData(res.data.item[0]);
+    };
+
+    // 전체 데이터
+    const repliesData = async () => {
+      const res = await axios.get(`https://localhost/api/replies/all`, {
+        headers: {
+          Authorization: `Bearer ${AUTH_TOKEN()}`,
+        },
+      });
+
+      setData(res.data.item);
+      setPrevData(res.data.item[dataId - 1]);
+      setNextData(res.data.item[dataId + 1]);
+    };
+
+    repliesCurrentData();
+    repliesData();
+  }, [dataId, id, setCurrentData]);
+
   return (
     <>
       <Helmet>
-        <title>{current.title}</title>
+        {currentData && (
+          <title>
+            {currentData.title ? currentData.title : currentData.content}
+          </title>
+        )}
       </Helmet>
 
       <div>
         <PageMap route="qna" category="상품 Q&A" />
         <PageDetailTitle title="상품 Q&A" explan="상품 Q&A입니다." />
-        {current.productId && (
+        {currentData && currentData.product && (
           <ReviewProductItem
-            link={`/detail/${current.productId}`}
-            thumbnail={current.productImg}
-            name={current.productName}
-            price={current.productPrice}
+            link={`/detail/${currentData.product._id}`}
+            thumbnail={currentData.product.image}
+            name={currentData.product.name}
           />
         )}
-        <PageDetailTable
-          title={current.title}
-          writer={current.writer}
-          grade={current.grade}
-          date={current.date}
-          attachFile={current.attachFile}
-          content={current.content}
-        />
-        <DetailButton
-          btn1="목록"
-          btn3="삭제"
-          onClick1={() => navigate('/qna')}
-          onClick3={handleDelete}
-          style="quaReviewDetailButton"
-          center="center"
-          writer={current.writer}
-        />
+        {currentData && (
+          <PageDetailTable
+            title={currentData.title ? currentData.title : currentData.content}
+            writer={currentData.user.name}
+            createdAt={currentData.createdAt}
+            attachFile={currentData.attachFile ? currentData.attachFile : ''}
+            content={currentData.content}
+          />
+        )}
+        {currentData && (
+          <DetailButton
+            btn1="목록"
+            btn3="삭제"
+            onClick1={() => navigate('/qna')}
+            onClick3={handleDelete}
+            style="quaReviewDetailButton"
+            center="center"
+            writer={currentData.user.name}
+          />
+        )}
 
         {idFilterComment &&
           idFilterComment.map((v, i) => (
@@ -112,13 +138,14 @@ function QnaDetail() {
           ))}
 
         {userInfo && <CommentInput writer={userInfo.name} collection="qna" />}
+
         <PageListOrder
-          prev={prev ? prev.title : ''}
-          next={next ? next.title : ''}
-          prevLink={prev ? `/qna-detail/${prev._id}` : ''}
-          nextLink={next ? `/qna-detail/${next._id}` : ''}
+          prev={prevData?.title ? prevData.title : prevData?.content}
+          next={nextData?.title ? nextData.title : nextData?.content}
+          prevLink={prevData ? `/qna-detail/${prevData._id}` : ''}
+          nextLink={nextData ? `/qna-detail/${nextData._id}` : ''}
           _id={Number(id)}
-          length={length}
+          length={data.length}
         />
       </div>
     </>
