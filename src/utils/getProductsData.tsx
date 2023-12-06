d159cc22e4fe7a54035d35341f0c436db832f57e
KIMGEUNDU <vlkoklv@naver.com>
2023-12-06 17:34:03 +0900

    ts: 타입 수정

--- src/utils/getProductsData.tsx
+++ src/utils/getProductsData.tsx
@@ -1,3 +1,3 @@
-export function sortQnaReviewData(data: QnaReviewData[]) {
+export function sortQnaReviewData(data: Replies[]) {
   return data.slice().sort((a, b) => Number(b._id) - Number(a._id));
 }
