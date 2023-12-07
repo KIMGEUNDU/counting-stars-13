f5bbb9fa18d3bf6ad008dd9aeb03cd5d622536aa
KIMGEUNDU <vlkoklv@naver.com>
2023-12-07 17:37:02 +0900

    docs: 관리자 계정일시 별해달 나오게 수정

--- src/components/QnA,Review/CommentItem.tsx
+++ src/components/QnA,Review/CommentItem.tsx
@@ -2,7 +2,9 @@ function CommentItem({ writer, createdAt, content }: Replies) {
   return (
     <div className="center border border-gray-300 text-sm my-3">
       <div className="flex gap-4 border-b border-b-gray-300 p-3">
-        <span className="font-semibold">{writer}</span>
+        <span className="font-semibold">
+          {writer === '무*' ? '별해달' : writer}
+        </span>
         <span>{createdAt}</span>
       </div>
       <p className="p-3">{content}</p>
