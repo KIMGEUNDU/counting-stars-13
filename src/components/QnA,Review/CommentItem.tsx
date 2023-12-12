d7b714d312dbaaa99b6b2718d3e5c73c061c0b34
KIMGEUNDU <vlkoklv@naver.com>
2023-12-12 17:27:19 +0900

    feat: 수정, 삭제 기능 추가

--- src/components/QnA,Review/CommentItem.tsx
+++ src/components/QnA,Review/CommentItem.tsx
@@ -1,13 +1,48 @@
-function CommentItem({ writer, createdAt, content }: Replies) {
+import { useUserInfo } from '@/store/useUserInfo';
+import { setAnonymousName } from '@/utils/setAnonymousName';
+
+function CommentItem({
+  writer,
+  createdAt,
+  content,
+  onEdit,
+  onDelete,
+  onEditComplete,
+  writerId,
+  edit,
+}: Replies & {
+  onEdit: () => void;
+  onDelete: () => void;
+  onEditComplete: () => void;
+  writerId: number;
+  edit: boolean;
+}) {
+  const { userInfo } = useUserInfo();
+
   return (
     <div className="center border border-gray-300 text-sm my-3">
-      <div className="flex gap-4 border-b border-b-gray-300 p-3">
-        <span className="font-semibold">
-          {writer === '무*' ? '별해달' : writer}
-        </span>
-        <span>{createdAt}</span>
+      <div className="flex justify-between border-b border-b-gray-300 p-3">
+        <div className="flex gap-4">
+          <span className="font-semibold">{setAnonymousName(writer)}</span>
+          <span>{createdAt}</span>
+        </div>
+        {writerId === userInfo!._id && edit && (
+          <div className="flex gap-2">
+            <button type="button" onClick={onEdit}>
+              <img className="w-4" src="/editIcon.png" alt="댓글 수정" />
+            </button>
+            <button type="button" onClick={onDelete}>
+              <img className="w-5" src="/deleteIcon.png" alt="댓글 삭제" />
+            </button>
+          </div>
+        )}
+        {writerId === userInfo!._id && !edit && (
+          <button type="button" onClick={onEditComplete}>
+            <img className="w-5" src="/completeIcon.png" alt="수정 완료" />
+          </button>
+        )}
       </div>
-      <p className="p-3">{content}</p>
+      <input className="p-3" value={content} disabled={edit} />
     </div>
   );
 }
