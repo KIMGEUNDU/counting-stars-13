be8d1f41a3f0265c54515aa6e89601cc09efc692
KIMGEUNDU <vlkoklv@naver.com>
2023-12-12 17:28:15 +0900

    ts: qna 댓글 인터페이스 추가

--- src/store/useComment.ts
+++ src/store/useComment.ts
@@ -4,10 +4,16 @@ interface useComment {
   comment: Replies[];
   setComment: (comment: Replies) => void;
   setDeleteComment: (qna: Replies[]) => void;
+  qnaComment: Replies[];
+  setQnaComment: (qnaComment: Replies) => void;
+  setDeleteQnaComment: (qna: Replies[]) => void;
 }
 
 export const useComment = create<useComment>((set) => ({
   comment: [],
   setComment: (comment) => set((state) => ({ comment: [...state.comment, comment] })),
   setDeleteComment: (comment) => set({ comment }),
+  qnaComment: [],
+  setQnaComment: (qnaComment) => set((state) => ({ qnaComment: [...state.qnaComment, qnaComment] })),
+  setDeleteQnaComment: (qnaComment) => set({ qnaComment }),
 }));
