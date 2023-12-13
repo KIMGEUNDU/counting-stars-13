0f1b7e52df9ca0b2afa21a768f1a814f5d56439a
KIMGEUNDU <vlkoklv@naver.com>
2023-12-13 17:39:32 +0900

    feat: 댓글 수정, 삭제 기능

--- src/components/QnA,Review/CommentItem.tsx
+++ src/components/QnA,Review/CommentItem.tsx
@@ -1,23 +1,66 @@
 import { useUserInfo } from '@/store/useUserInfo';
+import { AUTH_TOKEN } from '@/utils/AUTH_TOKEN';
 import { setAnonymousName } from '@/utils/setAnonymousName';
+import axios from 'axios';
+import { ChangeEvent, KeyboardEvent, useState } from 'react';
+import toast from 'react-hot-toast';
+import { useParams } from 'react-router-dom';
 
 function CommentItem({
   writer,
   createdAt,
   content,
-  onEdit,
   onDelete,
-  onEditComplete,
   writerId,
-  edit,
+  commentId,
+  status,
+  setStatus,
 }: Replies & {
-  onEdit: () => void;
-  onDelete: () => void;
-  onEditComplete: () => void;
-  writerId: number;
-  edit: boolean;
+  onDelete?: () => void;
+  status?: boolean | undefined;
+  setStatus?: (status: boolean) => void;
+  writerId?: number;
+  commentId?: number;
 }) {
+  const [edit, setEdit] = useState(true);
   const { userInfo } = useUserInfo();
+  const [inputContent, setInputContent] = useState('');
+  const { id } = useParams();
+
+  // 댓글 수정하기 active 이벤트
+  const handleEditComment = () => setEdit(false);
+
+  // 댓글 내용 바꾸기
+  const handleChangeContent = (
+    e: ChangeEvent<HTMLInputElement> | KeyboardEvent<HTMLInputElement>
+  ) => {
+    setInputContent((e as ChangeEvent<HTMLInputElement>).target.value);
+  };
+
+  // 댓글 수정 이벤트
+  const handleEditCompleteComment = async () => {
+    const res = await axios.patch(
+      `https://localhost/api/posts/${id}/replies/${commentId}`,
+      { content: inputContent },
+      {
+        headers: {
+          Authorization: `Bearer ${AUTH_TOKEN()}`,
+        },
+      }
+    );
+
+    if (res.data.ok === 1) {
+      toast('수정되었습니다 :)', {
+        icon: '✏️',
+        duration: 2000,
+      });
+
+      setEdit(true);
+      if (setStatus) {
+        setStatus(!status);
+      }
+    }
+  };
 
   return (
     <div className="center border border-gray-300 text-sm my-3">
@@ -26,9 +69,9 @@ function CommentItem({
           <span className="font-semibold">{setAnonymousName(writer)}</span>
           <span>{createdAt}</span>
         </div>
-        {writerId === userInfo!._id && edit && (
+        {writerId === userInfo?._id && edit && (
           <div className="flex gap-2">
-            <button type="button" onClick={onEdit}>
+            <button type="button" onClick={handleEditComment}>
               <img className="w-4" src="/editIcon.png" alt="댓글 수정" />
             </button>
             <button type="button" onClick={onDelete}>
@@ -36,13 +79,26 @@ function CommentItem({
             </button>
           </div>
         )}
-        {writerId === userInfo!._id && !edit && (
-          <button type="button" onClick={onEditComplete}>
+        {writerId === userInfo?._id && !edit && (
+          <button type="button" onClick={handleEditCompleteComment}>
             <img className="w-5" src="/completeIcon.png" alt="수정 완료" />
           </button>
         )}
       </div>
-      <input className="p-3" value={content} disabled={edit} />
+      {!edit && (
+        <input
+          className={`p-3 w-full ${!edit ? 'bg-amber-50' : ''}`}
+          defaultValue={content}
+          disabled={edit}
+          onChange={handleChangeContent}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleEditCompleteComment();
+            }
+          }}
+        />
+      )}
+      {edit && <p className="p-3 w-full">{content}</p>}
     </div>
   );
 }
