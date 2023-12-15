2d1af59af34e9565a814f8bc8c7533cfde525b10
rustandbone <dlehdgh14@naver.com>
2023-12-15 17:29:56 +0900

    markup: tbody 안에 들어간 thead 삭제

--- src/components/MyOrder/OrderItem.tsx
+++ src/components/MyOrder/OrderItem.tsx
@@ -12,29 +12,27 @@ export default function OrderItem({
   orderDate = orderDate?.slice(0, 10);
 
   return (
-    <thead>
-      <tr className="h-[90px] border-b">
-        <td className="bg-gray-50">
-          <span className="text-sm">
-            {orderDate} <br />
-          </span>
-        </td>
-        <td colSpan={6}>
-          {Object.values(productList).map((item, i) => (
-            <div key={i} className="flex items-center">
-              <OrderItemDetail
-                key={i}
-                link={item._id ? item._id : ''}
-                img={item.image}
-                name={item.name}
-                number={item.quantity ? item.quantity : 0}
-                price={item.price}
-                deliveryStateNum={item.state ? item.state : ''}
-              />
-            </div>
-          ))}
-        </td>
-      </tr>
-    </thead>
+    <tr className="h-[90px] border-b">
+      <td className="bg-gray-50">
+        <span className="text-sm">
+          {orderDate} <br />
+        </span>
+      </td>
+      <td colSpan={6}>
+        {Object.values(productList).map((item, i) => (
+          <div key={i} className="flex items-center">
+            <OrderItemDetail
+              key={i}
+              link={item._id ? item._id : ''}
+              img={item.image}
+              name={item.name}
+              number={item.quantity ? item.quantity : 0}
+              price={item.price}
+              deliveryStateNum={item.state ? item.state : ''}
+            />
+          </div>
+        ))}
+      </td>
+    </tr>
   );
 }
