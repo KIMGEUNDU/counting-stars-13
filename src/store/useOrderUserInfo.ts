e703fb3eaeea17530b2e92bd521ce3a288bbd75f
rustandbone <dlehdgh14@naver.com>
2023-12-19 17:30:31 +0900

    feat: 주문 페이지 주소 persist 추가

--- src/store/useOrderUserInfo.ts
+++ src/store/useOrderUserInfo.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface orderUserInfo {
   email: string;
@@ -15,19 +16,24 @@ interface orderUserInfoState {
   setAddress: (isAddress: address | undefined) => void;
 }
 
-export const useOrderUserInfo = create<orderUserInfoState>((set) => ({
-  orderUserInfo: {
-    email: '',
-    name: '',
-    phone: '',
-    address: { zonecode: '', address: '', addressDetail: '' },
-    subAddress: { zonecode: '', address: '', addressDetail: '' },
-  },
-  setOrderUserInfo: (orderUserInfo) => set({ orderUserInfo }),
-  isAddress: {
-    zonecode: '',
-    address: '',
-    addressDetail: '',
-  },
-  setAddress: (isAddress) => set({ isAddress }),
-}));
+export const useOrderUserInfo = create(
+  persist<orderUserInfoState>(
+    (set) => ({
+      orderUserInfo: {
+        email: '',
+        name: '',
+        phone: '',
+        address: { zonecode: '', address: '', addressDetail: '' },
+        subAddress: { zonecode: '', address: '', addressDetail: '' },
+      },
+      setOrderUserInfo: (orderUserInfo) => set({ orderUserInfo }),
+      isAddress: {
+        zonecode: '',
+        address: '',
+        addressDetail: '',
+      },
+      setAddress: (isAddress) => set({ isAddress }),
+    }),
+    { name: 'user-info-storage' }
+  )
+);
